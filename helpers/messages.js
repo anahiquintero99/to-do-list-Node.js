7fd4e538e295ec7286b20346872ba05c08ebe200
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-12 21:33:39 -0500

    print option to menu

--- helpers/messages.js
+++ helpers/messages.js
@@ -1,37 +1,42 @@
 require('colors');
 
 const showMenu = () => {
-  console.clear();
-  console.log(' ==============================='.green);
-  console.log('       Select an option: '.green);
-  console.log(' ===============================\n'.green);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log(' ==============================='.green);
+    console.log('       Select an option: '.green);
+    console.log(' ===============================\n'.green);
 
-  console.log(`${'1'.green} Crear Tarea`);
-  console.log(`${'2'.green} Listar Tareas`);
-  console.log(`${'3'.green} Listar Tareas Completadas`);
-  console.log(`${'4'.green} Listar Tareas Pendientes`);
-  console.log(`${'5'.green} Completar Tarea(S)`);
-  console.log(`${'6'.green} Borrar Tarea`);
-  console.log(`${'0'.green} Salir \n`);
+    console.log(`${'1'.green} Crear Tarea`);
+    console.log(`${'2'.green} Listar Tareas`);
+    console.log(`${'3'.green} Listar Tareas Completadas`);
+    console.log(`${'4'.green} Listar Tareas Pendientes`);
+    console.log(`${'5'.green} Completar Tarea(S)`);
+    console.log(`${'6'.green} Borrar Tarea`);
+    console.log(`${'0'.green} Salir \n`);
 
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question('Select an option: ', (opt) => {
-    readline.close();
+    readline.question('Select an option: ', (opt) => {
+      readline.close();
+    });
   });
 };
 
 const pause = () => {
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question(`\n Presione ${'ENTER'.green} para continuar\n`, (opt) => {
-    readline.close();
+    readline.question(`\n Presione ${'ENTER'.green} para continuar\n`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
 
