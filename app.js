3eca474a4b51613b8ad0ec3ba62597e46c239e5c
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-12 20:44:53 -0500

    Add menu

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//terminal packages
 require('colors');
+
+//our packages
+const { showMenu, pause } = require('./helpers/messages');
+
 console.clear();
 
 const main = async () => {
   console.log('Hola Mundo');
+  showMenu();
+  pause();
 };
 
 main();
