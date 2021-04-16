aa2df5acf9439eff273bb452ee5cd0eb77e30fb7
Anahi Quintero <anitaquintero99@gmail.com>
2021-04-15 20:39:13 -0500

    Created new fuction to add content to list

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //terminal packages
 require('colors');
 
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
 const Chores = require('./models/chores');
 
 //main
@@ -9,11 +9,23 @@ const main = async () => {
   console.log('Hola Mundo');
 
   let opt = '';
+  const tasks = new Chores();
 
   //show menu
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+
+    switch (opt) {
+      case '1':
+        //created option
+        const desc = await readInput('Descripción: ');
+        tasks.createTasks(desc);
+        break;
+
+      case '2':
+        console.log(tasks._listado);
+        break;
+    }
 
     await pause();
   } while (opt !== '0');
--- models/chores.js
+++ models/chores.js
@@ -2,12 +2,18 @@
  * _listado:
  *    {'uuid-123712-123123-2: {id:12, desc:asd, completedIn:92231} },
  */
+const Homework = require('./homework');
 
 class Chores {
   _listado = {};
 
   constructor() {
     this._listado = {};
+  }
+
+  createTasks(desc = '') {
+    const task = new Homework(desc);
+    this._listado[task.id] = task;
   }
 }
 
